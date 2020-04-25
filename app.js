a979b25bf6416c56847b7c03ae962d4da21fc4a7
MarioEstebanOchoa <45211069+MarioEstebanOchoa@users.noreply.github.com>
2020-04-24 20:17:51 -0500

    Seccion 11 terminada, crud imagenes

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var bodyParser = require('body-parser');
 var appRoutes = require('./routes/app')
 var usuarioRoutes = require('./routes/usuario')
 var loginRoutes = require('./routes/login')
+var hospitalRoutes = require('./routes/hospital')
+var medicoRoutes = require('./routes/medico')
+var busquedaRoutes = require('./routes/busqueda')
+var uploadRoutes = require('./routes/upload')
+var imagenesRoutes = require('./routes/imagenes')
 
 
 // Inicializar varaibles
@@ -18,19 +23,27 @@ app.use(bodyParser.json())
 
 
 //Conexion a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    (err, res) => {
 
-    if (err) throw err;
+        if (err) throw err;
 
-    console.log('Base de datos: \x1b[36m%s\x1b[0m', 'online');
-
-
-})
+        console.log('Base de datos: \x1b[36m%s\x1b[0m', 'online');
+    })
 
 
 //Rutas
-app.use('/usuario', usuarioRoutes);
 app.use('/login', loginRoutes);
+app.use('/usuario', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
 app.use('/', appRoutes);
 
 
